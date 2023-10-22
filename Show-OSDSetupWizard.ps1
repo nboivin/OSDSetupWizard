026005c16d8ac05abc9a11e81ded9c2e03b215ad
Nicolas Boivin <nicolas@boivin.io>
2023-10-22 16:39:05 -0400

    Initial commit

---
+++ Show-OSDSetupWizard.ps1
@@ -0,0 +1,110 @@
+﻿[System.Reflection.Assembly]::LoadWithPartialName('System.Windows.Forms')| out-null
+[System.Reflection.Assembly]::LoadWithPartialName('presentationframework') | out-null
+
+# Ref : https://github.com/gbdixg/Show-Win32AppUI/blob/main/Show-Win32AppUI/Show-Win32AppUI.ps1
+
+# Track currentPage
+$script:CurrentPage = 0
+
+$script:HasError = $false
+
+
+function LoadXml($filename) {
+    $xmlLoader = New-Object System.Xml.XmlDocument
+    $xmlLoader.Load($filename)
+    return $xmlLoader
+}
+
+# Load XAML files
+$xmlMainWindow = LoadXml("$PSScriptRoot\Xaml\MainWindow.xaml")
+$xmlConfig = LoadXml("$PSScriptRoot\Config.xml")
+
+# Collection storing references to all named WPF controls in the UI
+$UIControls=[hashtable]::Synchronized(@{})
+
+# Convert Windows and Pages to a XAML object graph
+$UIControls.MainWindow = [Windows.Markup.XamlReader]::Load((New-Object -TypeName System.Xml.XmlNodeReader -ArgumentList $xmlMainWindow))
+
+
+# Load page enabled in the config file
+$EnabledPages = @()
+foreach ($page in $xmlConfig.wizard.Page) {
+
+    if ($page.Enabled -eq $True) {
+        "Loading page $($page.id)"
+        $pageXml = LoadXml("$PSScriptRoot\Xaml\$($page.filename)")
+        $UIControls.$($page.id) = [Windows.Markup.XamlReader]::Load((New-Object -TypeName System.Xml.XmlNodeReader -ArgumentList $pageXml))
+        $pageXml.SelectNodes("//*[@*[contains(translate(name(.),'n','N'),'Name')]]") | ForEach-Object -Process {
+            $UIControls.$($_.Name) = $UIControls.$($page.id).FindName($_.Name)
+        }
+
+        if ($page.script)
+        {
+            . "$PSScriptRoot\scripts\$($page.script)"
+        }
+
+        $EnabledPages += $($page.id)
+    }
+}
+
+# Load the main window
+$XmlMainWindow.SelectNodes("//*[@*[contains(translate(name(.),'n','N'),'Name')]]") | ForEach-Object -Process {
+    $UIControls.$($_.Name) = $UIControls.MainWindow.FindName($_.Name)
+}
+
+
+$UIControls.btn_Next.Add_Click({
+    
+    if ($script:HasError -eq $false) {
+        $script:CurrentPage++
+        $UIControls.frame_Pages.Content = $UIControls.$($EnabledPages[$script:CurrentPage])
+        $UIControls.Btn_Previous.IsEnabled = $true
+    
+        if (-Not ($EnabledPages[$script:CurrentPage + 1])) {
+            $UIControls.btn_Next.IsEnabled = $false
+            $UIControls.Btn_Finish.IsEnabled = $true
+        }
+    }
+    
+    
+})
+
+$UIControls.Btn_Previous.Add_Click({
+    
+    $script:CurrentPage--
+    $UIControls.frame_Pages.Content = $UIControls.$($EnabledPages[$script:CurrentPage])
+    $UIControls.btn_Next.IsEnabled = $true
+    $UIControls.Btn_Finish.IsEnabled = $false
+    
+    if ($script:CurrentPage -eq 0) {
+        $UIControls.Btn_Previous.IsEnabled = $false
+    }
+
+})
+
+
+$UIControls.btn_Cancel.Add_Click({
+    
+    $Result = [System.Windows.MessageBox]::Show('Are you sure you want to cancel ?', 'Cancel Wizard', 'YesNo','Question')
+
+    If ($Result -eq 'Yes') {
+        $UIControls.MainWindow.Close()
+    }
+
+})
+
+# Show first enabled page
+if ($EnabledPages[0]) {
+    $UIControls.frame_Pages.Content = $UIControls.$($EnabledPages[0])
+    $UIControls.Btn_Previous.IsEnabled = $false
+}
+
+# If no second page enabled
+if (-Not ($EnabledPages[1])) {
+    $UIControls.Btn_Previous.IsEnabled = $false
+    $UIControls.Btn_Next.IsEnabled = $false
+    $UIControls.Btn_Finish.IsEnabled = $true
+}
+
+# Show the user interface
+$UIControls.MainWindow.ShowDialog() | Out-Null
---
+++ scripts/computerdetails.ps1
@@ -0,0 +1,62 @@
+Write-host "Computer Details Script loaded..."
+
+$domainList = $($xmlConfig.wizard.Page | Where-Object {$_.id -eq "ComputerDetails"}).Domain
+
+function Import-DomainList {
+    
+    foreach($domain in $domainList) {
+        $UIControls.domainName.Items.Add("$($domain.Name)") | Out-Null
+    }
+
+    $UIControls.domainName.SelectedIndex = 0
+}
+
+function Import-DomainOUList($domainName) {
+
+    $UIControls.domainOU.Items.Clear()
+
+    $domainOUList = $($domainList | Where-Object {$_.Name -eq $domainName}).DomainOU
+
+    foreach ($domainOU in $domainOUList) {
+        $UIControls.domainOU.Items.Add($($domainOU.Name))
+    }
+
+    $UIControls.domainOU.SelectedIndex = 0
+
+}
+
+
+$UIControls.computerName.Add_Textchanged({ 
+    $_sender = $args[0]
+
+    if ($_sender.Text -match "^[-_]|[^a-zA-Z0-9-_]")
+    {
+        $script:HasError = $true
+        $UIControls.lb_ComputerDetailError.Visibility = "Visible"
+        $UIControls.lb_ComputerDetailError.Content = "The computer name is not valid."
+    }
+    elseif ($_sender.Text.Length -gt 15) {
+        $script:HasError = $true
+        $UIControls.lb_ComputerDetailError.Visibility = "Visible"
+        $UIControls.lb_ComputerDetailError.Content = "The computer name is longer than 15 characters."
+    }
+    elseif ($_sender.Text.Length -eq 0) {
+        $script:HasError = $true
+        $UIControls.lb_ComputerDetailError.Visibility = "Visible"
+        $UIControls.lb_ComputerDetailError.Content = "The computer name is required. Please enter a computer name."
+    }
+    else {
+        $script:HasError = $false
+        $UIControls.lb_ComputerDetailError.Visibility = "Collapsed"
+    }
+
+})
+
+$UIControls.domainName.Add_SelectionChanged({
+    $_sender = $args[0]
+
+    Import-DomainOUList -domainName $($_sender.SelectedItem)
+
+})
+
+Import-DomainList
