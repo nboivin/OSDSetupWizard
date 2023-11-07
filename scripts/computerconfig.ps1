10953539e37b4528f7e70d36f521a61a88796bc5
nboivin <54825758+nboivin@users.noreply.github.com>
2023-11-06 19:08:22 -0500

    Initial computer config page.

---
+++ scripts/computerconfig.ps1
@@ -0,0 +1,25 @@
+$configTypes = $($xmlConfig.wizard.Page | Where-Object {$_.id -eq "ComputerConfig"}).ConfigType
+
+
+function Import-ConfigType {
+
+    foreach($configType in $configTypes) {
+        $UIControls.configType.Items.Add("$($configType.Name)") | Out-Null
+    }
+
+    $UIControls.configType.SelectedIndex = 0
+}
+
+
+$UIControls.configType.Add_SelectionChanged({
+    $_sender = $args[0]
+
+    $description = ($configTypes | Where-Object {$_.Name -eq $_sender.SelectedItem}).Description
+    if ($description) {
+        $UIControls.lb_configTypeDescription.Content = "$description"
+    } else {
+        $UIControls.lb_configTypeDescription.Content = "No description"
+    }
+})
+
+Import-ConfigType
